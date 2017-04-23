f023a365b3858a873884e071f844568f24dadd69
Pascal Steichen <pst@libre.lu>
2017-04-23 17:14:42 +0200

    applied initial migrate

--- cms_lts/wsgi.py
+++ cms_lts/wsgi.py
@@ -7,7 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# set sys.path to the directory containing the django app
+# else wsgi will not find the settings file below
+sys.path.append('/var/www/aperta.lu/django')
+sys.path.append('/var/www/aperta.lu/django/lts-cms')
 
 from django.core.wsgi import get_wsgi_application
 
